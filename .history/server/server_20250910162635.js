7362c9437c6254a28a16c0f7f01fdfbe2a43fb8f
Zeina Wady <zeinawady@gmail.com>
2025-09-10 17:40:34 +0300

    change express version

--- .history/server/server_20250910162635.js
+++ .history/server/server_20250910162635.js
@@ -42,7 +42,7 @@ app.use("/order", orderRoutes);
 app.use("/userProfile",userRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
