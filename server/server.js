2bf658d11a4d43f36cc2d6ff505839286d1e5a0d
HabibaYossre <queenrody24@gmail.com>
2025-09-12 17:04:31 +0300

    add .env files

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(cors({
     // origin: "http://localhost:3001",  // <-- your React frontend URL
     // credentials: true   
     //          
-      origin: "https://flower-store-front.vercel.app", 
+      origin:"https://flower-store-front.vercel.app", 
       credentials: true
   }));
 
